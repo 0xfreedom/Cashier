bedbcf91661e4edfefa0e4bdb32fc0b3da2550a2
Ap0109i2e <liutingwei3996@163.com>
2015-08-30 23:50:46 +0800

    Revert "Revert "UpDate SqlQueryForm & UpDate DataBase Backup.""
    This reverts commit 09fd95fad3642b0ab61ce4945599dd98e1998341.

--- Cashier/Cashier_Main.cs
+++ Cashier/Cashier_Main.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -81,5 +82,66 @@ namespace Cashier
             aboutForm.Close();
             aboutForm.Dispose();
         }
+
+        private void 数据备份ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            if (File.Exists(AccessHelper.app_path + "\\db.mdb"))
+            {
+
+
+                saveFileDialog1.FileName = "db.mdb"; 
+                saveFileDialog1.Filter = "(*.mdb)|*.mdb";
+                saveFileDialog1.DefaultExt = "*.mdb";
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+
+                    //string newDBName = Guid.NewGuid() + ".mdb";
+                    //File.Copy(AccessHelper.app_path + "\\db.mdb", AccessHelper.app_path + "\\" + newDBName);
+
+                    //if (File.Exists(AccessHelper.app_path + "\\" + newDBName))
+                    //{
+
+                    // File.Copy(AccessHelper.app_path + "\\" + newDBName, saveFileDialog1.FileName, true);
+                    //}
+                    if (File.Exists(AccessHelper.app_path + "\\db.mdb"))
+                    {
+                        File.Copy(AccessHelper.app_path + "\\db.mdb", saveFileDialog1.FileName, true);
+                    }
+
+                    MessageBox.Show("备份数据成功！  \n  保存位置是: \n  " + saveFileDialog1.FileName);
+                }
+
+
+
+
+
+            }
+        }
+
+        private void 数据还原ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "(*.mdb)|*.mdb";
+            openFileDialog1.DefaultExt = "*.mdb";
+            openFileDialog1.ShowDialog();
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            if (File.Exists(openFileDialog1.FileName))
+            {
+                if (openFileDialog1.FileName.ToString().Contains("db.mdb"))
+                {
+
+                    File.Copy(openFileDialog1.FileName, AccessHelper.app_path + "\\db.mdb", true);
+                    MessageBox.Show("数据还原成功！ ");
+                    return;
+                }
+            }
+            MessageBox.Show("数据还原失败！请检查文件是否为db.mdb文件 ");
+        }
+
+
     }
 }
