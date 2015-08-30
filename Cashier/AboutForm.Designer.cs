09fd95fad3642b0ab61ce4945599dd98e1998341
Ap0109i2e <liutingwei3996@163.com>
2015-08-30 23:46:05 +0800

    Revert "UpDate SqlQueryForm & UpDate DataBase Backup."
    This reverts commit 241b25e702a2d8963045146d65771843dc7cd65e.

--- Cashier/AboutForm.Designer.cs
+++ Cashier/AboutForm.Designer.cs
@@ -33,7 +33,6 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -73,23 +72,12 @@
             this.label4.TabIndex = 2;
             this.label4.Text = "Thank You!@";
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(31, 203);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "SQL Query";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // AboutForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -110,6 +98,5 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button1;
     }
 }
--- Cashier/AboutForm.cs
+++ Cashier/AboutForm.cs
@@ -15,14 +15,5 @@ namespace Cashier
         {
             InitializeComponent();
         }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            SqlQuery sq = new SqlQuery();
-            sq.ShowDialog();
-            sq.Close();
-            sq.Dispose();
-
-        }
     }
 }
